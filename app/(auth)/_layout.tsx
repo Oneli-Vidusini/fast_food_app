430eac8cd1f63b2cacbe42af96ce03e7bbc2d320
Oneli Vidusini <onelividusini31@gmail.com>
2025-09-02 13:11:48 +0530

    sign in & up

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,35 @@
-import {View, Text} from 'react-native'
+import {View, Text, KeyboardAvoidingView, Platform, Animated, Dimensions} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {Slot} from "expo-router";
+import ScrollView = Animated.ScrollView;
+import {Image, ImageBackground} from "expo-image";
+import {images} from "@/constants";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 
 export default function _Layout (){
     return (
-        <SafeAreaView>
-            <Text>Auth Layout</Text>
-            <Slot/>
-        </SafeAreaView>
+        <KeyboardAvoidingView behavior={Platform.OS==='ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{height:Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode={"stretch"}/>
+                    <Image source={images.logo} className="self-center size-48 absolute-bottom-16 z-10" />
+                </View>
+
+                <CustomInput
+                    placeholder="Enter your email"
+                    value={''}
+                    onChangeText={(text) =>{}}
+                    label="Email"
+                    keyboardType="email-address"
+                />
+                <CustomButton />
+
+                <Slot />
+            </ScrollView>
+
+        </KeyboardAvoidingView>
 
     )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export  const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.jsm.fastfood",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68b56ed00007f9e85730',
+    userCollectionId:'user',
+}
